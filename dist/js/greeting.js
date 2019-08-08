66bea2e60f19478525e887b040980d46d72b95b5
seth greco <eug119@my.utsa.edu>
2019-08-08 02:11:29 -0500

    smooth scrolling added onClick for navigation menu. Jquery CDN used.

--- dist/js/greeting.js
+++ dist/js/greeting.js
@@ -14,3 +14,19 @@ window.onload = () => {
     y.innerHTML = " Good Evening";
   }
 };
+
+// Smooth Scrolling
+$("#main-nav a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
